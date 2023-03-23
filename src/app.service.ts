e85737de55b7216257f966f7963007fd115a2ea1
Xenomorphing79 <gourabb52@gmail.com>
2023-03-23 18:44:15 +0530

    Connected

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Testing...1..2..3..4..5';
+    return 'Connected!';
   }
 }
