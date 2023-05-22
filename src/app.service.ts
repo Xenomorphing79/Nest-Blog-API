eeea4f2cecb2ea8809f8e064a01c449517b5ac83
Xenomorphing79 <gourabb52@gmail.com>
2023-05-23 01:07:36 +0530

    Updated

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Connected!';
+    return 'This is a backend project, please refer to github to know how to use it.';
   }
 }
